ca0050f589063077ec3b826aaf045f796a7f0d77
adaydan-tabsdata <daniel@tabsdata.com>
2025-12-11 13:32:22 -0800

    push

--- src/td_sync/cancel_flows.py
+++ src/td_sync/cancel_flows.py
@@ -1,21 +1,22 @@
 import tabsdata as td
 import os
 from tabsdata.api.tabsdata_server import TabsdataServer
+from tabsdata.api import apiserver
 import inspect
 import sys
 import re
 
 
-
 def main():
     server = TabsdataServer("127.0.0.1:2457", "admin", "tabsdata", "sys_admin")
     transactions = server.list_transactions()
-    stalled_transactions = [i for i in transactions if i.status in ['Stalled', 'Running']]
+    server.dow
+    stalled_transactions = [
+        i for i in transactions if i.status in ["Stalled", "Running"]
+    ]
     if len(stalled_transactions) > 0:
         canceled_transactions = [i.cancel() for i in stalled_transactions]
-        print(f'Canceled {len(canceled_transactions)} stalled transactions')
+        print(f"Canceled {len(canceled_transactions)} stalled transactions")
         return canceled_transactions
-    print('No stalled transactions found')
+    print("No stalled transactions found")
     return []
-
-
---
+++ src/td_sync/download.py
@@ -0,0 +1,35 @@
+import tabsdata as td
+import os
+from tabsdata import api
+from tabsdata.api.tabsdata_server import TabsdataServer, APIServer
+import inspect
+import sys
+import re
+from pathlib import Path
+import polars as pl
+
+
+def download_table(
+    collection_name,
+    table_name,
+    socket: str = "127.0.0.1:2457",
+    username: str = "admin",
+    password: str = "tabsdata",
+    role: str = "sys_admin",
+):
+    CONFIG_DIR = Path(os.path.expanduser("~/.td_custom_extensions"))
+    CONFIG_DIR.mkdir(parents=True, exist_ok=True)
+    filepath = CONFIG_DIR / "temp_dir.parquet"
+
+    if filepath.exists():
+        filepath.unlink()
+
+    server = TabsdataServer(socket, username, password, role)
+    table = server.download_table(
+        collection_name=collection_name,
+        table_name=table_name,
+        destination_file=filepath,
+    )
+
+    tableframe = pl.read_parquet(filepath)
+    return tableframe

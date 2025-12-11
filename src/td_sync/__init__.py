d171ee238106dab1f0dba4ed4f3ce4c26c1cae9f
adaydan-tabsdata <daniel@tabsdata.com>
2025-12-11 13:36:35 -0800

    push

--- src/td_sync/__init__.py
+++ src/td_sync/__init__.py
@@ -1,6 +1,6 @@
 from .sync import main as sync_with_server
 from .cancel_flows import main as cancel_trx
 from .trigger import main as trigger_transaction
-from .download import main as download_table
+from .download import download_table
 
 __all__ = ["cancel_trx", "sync_with_server", "trigger_transaction", "download_table"]

32edfdbb7a7cedd0daa80b464db0165fdb4ebf24
adaydan-tabsdata <daniel@tabsdata.com>
2025-12-16 17:21:22 -0800

    commit

--- src/td_sync/cancel_flows.py
+++ src/td_sync/cancel_flows.py
@@ -1,16 +1,9 @@
-import tabsdata as td
-import os
 from tabsdata.api.tabsdata_server import TabsdataServer
-from tabsdata.api import apiserver
-import inspect
-import sys
-import re
 
 
 def main():
     server = TabsdataServer("127.0.0.1:2457", "admin", "tabsdata", "sys_admin")
     transactions = server.list_transactions()
-    server.dow
     stalled_transactions = [
         i for i in transactions if i.status in ["Stalled", "Running"]
     ]
